1ec80b8fda8c9f4777b1da9ecfc55660b19abef0
Magic elemental <vvlrff@mail.ru>
2023-04-04 22:01:14 +0300

    /

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } 
 
 import HomePage from './pages/HomePage'
 import { AboutPage } from './pages/AboutPage'
-import { postsLoader, PostsPage } from './pages/PostsPage'
+import { postsLoader, PostsPage } from './pages/ApplyingPage'
 import NotFoundPage from './pages/NotFoundPage'
-import {postLoader, SinglePage} from './pages/SinglePage'
+import {postLoader, SinglePage} from './pages/Dashboardpage'
 import LoginPage from './pages/LoginPage'
 
 import Layout from './components/Layout'
@@ -17,6 +17,9 @@ import './App.css'
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Layout />}>
     <Route index element={<HomePage />} />
+    <Route path='login' element={<LoginPage />} />
+    
+
     <Route path='about/*' element={<AboutPage />}>
       <Route path='contacts' element={<p>Our contacts</p>} />
       <Route path='team' element={<p>Our team</p>} />
@@ -27,7 +30,6 @@ const router = createBrowserRouter(createRoutesFromElements(
         <SinglePage />
       </RequireAuth>
     } loader={postLoader} />
-    <Route path='login' element={<LoginPage />} />
     <Route path='*' element={<NotFoundPage />} />
   </Route>
 ))
--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,10 +1,13 @@
 import { Link, Outlet } from 'react-router-dom'
 
+import "./../assets/css/Layout.css"
+
 const Layout = () => {
   return (
     <>
       <header>
         <Link to='/'>Home </Link>
+        <Link to='/login'>Login </Link>
         <Link to='/posts'>Posts </Link>
         <Link to='/about'>About </Link>
       </header>
