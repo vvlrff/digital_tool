39586fcc79849e2ee72eae79bb14294b691a4828
John Doe <vvlrff@mail.ru>
2023-04-08 20:03:15 +0300

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from 'react-router-dom'
 
-import HomePage from './pages/HomePage'
+import { HomePage } from './pages/HomePage'
 import { AboutPage } from './pages/AboutPage'
 import { postsLoader, PostsPage } from './pages/ApplyingPage'
-import NotFoundPage from './pages/NotFoundPage'
+import { NotFoundPage } from './pages/NotFoundPage'
 import { postLoader, SinglePage } from './pages/Dashboardpage'
-import LoginPage from './pages/LoginPage'
+import { LoginPage } from './pages/LoginPage'
 
 import Layout from './components/Layout'
 
@@ -17,18 +17,18 @@ import './App.css'
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Layout />}>
     <Route index element={<HomePage />} />
-    <RequireAuth>
-
-      <Route path='about/*' element={<AboutPage />}>
-        <Route path='contacts' element={<p>Our contacts</p>} />
-        <Route path='team' element={<p>Our team</p>} />
-      </Route>
-      
-      <Route path='posts' element={<PostsPage />} loader={postsLoader} />
-      <Route path='posts/:id' element={<SinglePage />} loader={postLoader} />
-
-    </RequireAuth>
+    <Route path='about/*' element={<AboutPage />}>
+      <Route path='contacts' element={<p>Our contacts</p>} />
+      <Route path='team' element={<p>Our team</p>} />
+    </Route>
+    <Route path='posts' element={<PostsPage />} loader={postsLoader} />
+    <Route path='posts/:id' element={
+      <RequireAuth>
+        <SinglePage />
+      </RequireAuth>
+    } loader={postLoader} />
     <Route path='login' element={<LoginPage />} />
+    <Route path='register' element={<LoginPage />} />
     <Route path='*' element={<NotFoundPage />} />
   </Route>
 ))
