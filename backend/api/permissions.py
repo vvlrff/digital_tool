796f214f83a8f5eba23478c69610eee6afc33b61
Никита Макарьев <nikitamakarev@MacBook-Pro-Tommy.local>
2023-04-10 21:39:42 +0300

    Update permissions for vacancy

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -8,3 +8,9 @@ class EmployerOnly(permissions.BasePermission):
 
         if request.user and request.user.is_employer:
             return True
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
